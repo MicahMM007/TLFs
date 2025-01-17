09c40cfd9916b2cc441dfeba332a36bc710b38be
Moses Micah Mayanja <micah@scigenix.ai>
2025-01-17 10:23:37 +0200

    update TLFs

--- R/TLFs.R
+++ R/TLFs.R
@@ -61,9 +61,9 @@ basic_table(show_colcounts = TRUE) %>%
   add_colcounts() %>%
   analyze_vars(
     vars = vars,
-    var_labels = vars_labels
-    #.stats = c("n","mean_sd","count"),
-    #.formats = c(mean_sd = "xx.xx (xx.xx)")
+    var_labels = vars_labels,
+    stats = c("n","mean_sd","count"),
+    formats = c(mean_sd = "xx.xx (xx.xx)")
   ) %>%
   build_table(adsl %>% dplyr::filter(COUNTRY == "CHN")) # country China
 
@@ -84,9 +84,94 @@ basic_table() %>%
   ) %>%
   build_table(df=adae)
 
+# a statistics function calculating th numbers in summarize_num_patients
+s_num_patients(x = adae$USUBJID, labelstr = "", .N_col = nrow(adae))
+
+# further analyse the unique adverse events
+basic_table() %>%
+  split_cols_by(var = "ACTARM") %>%
+  add_colcounts() %>%
+  add_overall_col(label = "All Patients") %>%
+  summarize_num_patients(
+    var = "USUBJID",
+    .stats = c("unique", "nonunique"),
+    .labels = c(
+      unique = "Total number of patients with at least one AE",
+      nonunique = "Overall total number of events"
+    )
+  ) %>%
+  split_rows_by(
+    "AEBODSYS",
+    child_labels = "visible",
+    nested = FALSE,
+    #indent_mod = -1L,
+    split_fun = drop_split_levels
+  ) %>%
+  summarize_num_patients(
+    var = "USUBJID",
+    .stats = c("unique","nonunique")
+  ) %>%
+  build_table(df=adae,
+              alt_counts_df = adsl)
 
 
+# further layout using count_occurrences()
 
+basic_table() %>%
+  split_cols_by("ACTARM") %>%
+  add_colcounts() %>%
+  add_overall_col(label = "ALL Patients") %>%
+  summarize_num_patients(
+    var = "USUBJID",
+    .stats = c("unique","nonunique"),
+    .labels = c(
+      unique = "Total number of patients with at least one AE",
+      nonunique = "Overall total number of events"
+    )
+  ) %>%
+  split_rows_by(
+    "AEBODSYS"
+    #split_fun = drop_split_levels
+  ) %>%
+  summarize_row_groups() %>% #or use summarize_num_patients(var,.stats,.labels)
+  count_occurrences(vars = "AEDECOD") %>%
+  build_table(df=adae,
+              alt_counts_df = adsl)
 
+# --- end
 
+# Response Table
+# __ Proportion of responders in each treatment group
+
+# preprocessing
+anl <- adrs %>%
+  dplyr::filter(PARAMCD == "BESRSPI") %>%
+  dplyr::mutate(is_rsp = AVALC %in% c("CR","PR"))
+
+basic_table() %>%
+  split_cols_by(var = "ARM", ref_group = "B: Placebo") %>%
+  add_colcounts() %>%
+  estimate_proportion(
+    vars = "is_rsp",
+    table_names = "est_prop",
+    conf_level = 0.9
+  ) %>%
+  build_table(anl)
+
+# --
+basic_table() %>%
+  split_cols_by(var = "ARM", ref_group = "B: Placebo") %>%
+  add_colcounts() %>%
+  estimate_proportion_diff( #compare to reference group
+    vars = "is_rsp",
+    conf_level = 0.9
+  ) %>%
+  build_table(anl)
+
+# --
+basic_table() %>%
+  split_cols_by(var = "ARM",ref_group = "B: Placebo") %>%
+  add_colcounts() %>%
+  test_proportion_diff(vars = "is_rsp") %>% #add method = "schouten"
+  build_table(anl)
 
